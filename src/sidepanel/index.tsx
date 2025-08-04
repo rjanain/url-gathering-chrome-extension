cf554c495535cc90751c6d69ea4ca4d1140a7d8c
Rakesh <jana.rakesh.math@gmail.com>
2025-08-04 11:49:46 +0530

    feat: Update sidepanel to use App component and enhance HTML structure

--- src/sidepanel/index.tsx
+++ src/sidepanel/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import SidePanelApp from './SidePanelApp'
+import App from '../popup/App'
+import '../styles/globals.css'
 
 const container = document.getElementById('sidepanel-root')
 if (container) {
   const root = createRoot(container)
-  root.render(<SidePanelApp />)
+  root.render(<App />)
 }
